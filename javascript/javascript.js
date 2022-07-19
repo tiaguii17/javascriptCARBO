77e0f89cb64114b4fe848f405f4daaffd57b0dce
tiago carbo <tiagocarbo1785@gmail.com>
2022-07-19 19:27:10 -0300

    funcion resuelta

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,13 @@
 
-
+function jugadores(){
 let player1 = parseInt(prompt("Ingrese una opcion:\n 1= Piedra\n 2= Papel\n 3= Tijera"))
 let player2 = parseInt(prompt("Ingrese una opcion:\n 1= Piedra\n 2= Papel\n 3= Tijera"))
+let resultado = juego(player1 , player2);
+return resultado;
+}
+  jugadores()
 
- 
-
-function juego(player1, player2) {
+function juego(player1, player2){
     let resultado = player1 + player2
     if(player1 == player2){
         alert("Empate");
@@ -13,19 +15,20 @@ function juego(player1, player2) {
         switch(resultado){
             case 3:
                 return alert("Gana Papel")
-            break;
+            
             case 4: 
                 return alert("Gana Piedra")
-            break;
+            
             case 5:
                 return alert("Gana Tijera")
-            break;
+            
         }
             return resultado;
     }
 }
 
-  console.log(juego(player1, player2))
+
+
 
  
 
