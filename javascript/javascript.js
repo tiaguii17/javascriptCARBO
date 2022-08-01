5d361f03b81fe6dbd6ffdab353584c33c409927a
tiago carbo <tiagocarbo1785@gmail.com>
2022-07-31 22:55:08 -0300

    al

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,49 +1,112 @@
 
 
-const elecciones = [];
-class Ppt{
-    constructor(num){
+const ELECCIONES = [];
+
+class Ppt {
+    constructor(num) {
         this.num = num
     }
 }
 
-function eleccion (){
+function eleccion() {
     let min = 1;
     let max = 3;
 
-    let player1 =new Ppt(parseInt(prompt("Ingrese una opcion:\n 1= Piedra\n 2= Papel\n 3= Tijera")));
-    
-    console.log(player1)
-    elecciones.push(player1);
-    let player2 = Math.floor(Math.random()*(max-min+1)+min);
-    elecciones.push(player2)
-        alert("la maquina escogio:" +" "+ player2);
-    let resultado = juego(player1 , player2);
-return resultado;
-}
-    eleccion();
+    let nombre = prompt("Ingrese su nombre: ");
 
-function juego(player1, player2){
-    let resultado = player1.num + player2
-    if(player1 == player2){
-          alert("Empate");
-    } else {
-        switch(resultado){
-            case 3:
-                return alert("Gana Papel");
-                
-            case 4: 
-                return alert("Gana Piedra");
-                
-            case 5:
-                return alert("Gana Tijera");
-                
-        }
-            return resultado;
-    }   
+
+    
+    for (let i = 0; i < 3; i++) {
+
+        let player1 = new Ppt(parseInt(prompt(`
+    Ingrese una opcion: 
+    1= Piedra 
+    2= Papel 
+    3= Tijera`)));
+
+        console.log(player1);
+
+        ELECCIONES.push(player1);
+
+        console.log(ELECCIONES);
+
+        let player2 = Math.floor(Math.random() * (max - min + 1) + min);
+
+        console.log(player2);
+
+        ELECCIONES.push(player2)
+
+        console.log(ELECCIONES);
+
+        alert(`la maquina escogio: ${player2}`);
+
+        juego(player1, player2, nombre)
+
+    }
 }
-    console.log(juego)
-    console.log(elecciones)
+eleccion();
+
+function juego(player1, player2, nombre) {
+
+    let miNumero = player1.num;
+
+    let jugador = 0;
+    let maquina = 0;
+
+    if (miNumero === 1 && player2 === 1) {
+        alert("empate");
+    }
+    else if (miNumero === 2 && player2 === 2) {
+        alert("empate");
+
+    } else if (miNumero === 3 && player2 === 3) {
+        alert("empate");
+    }
+    else if (miNumero === 1 && player2 === 2) {
+        maquina++;
+        alert("Gana la maquina");
+    }
+    else if (miNumero === 1 && player2 === 3) {
+        jugador++;
+        alert(`Gana ${nombre}`);
+    }
+    else if (miNumero === 2 && player2 === 1) {
+        jugador++;
+        alert(`Gana ${nombre}`);
+    }
+    else if (miNumero === 2 && player2 === 3) {
+        maquina++;
+        alert("Gana la maquina");
+    }
+    else if (miNumero === 3 && player2 === 1) {
+        maquina++;
+        alert("Gana la maquina");
+    }
+    else if (miNumero === 3 && player2 === 2) {
+        jugador++;
+        alert(`Gana ${nombre}`);
+    }
+
+    console.log(
+        `%c Jugador = ${jugador}`, 
+        `background: yellow; 
+            color: black; 
+            padding: 5px 6px 5px 0px; 
+            font-size: 15px;
+            border-radius: 5px;
+        `);
+    console.log(
+        `%c Maquina = ${maquina}`, 
+        `background: yellow; 
+            color: black; 
+            padding: 5px 6px 5px 0px; 
+            font-size: 15px;
+            border-radius: 5px;
+        `);
+
+    
+
+}
 
 
  
